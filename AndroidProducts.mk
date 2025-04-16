9f10e838c422acd8c5e974e453988b33b9610850
pola125 <3597599759@qq.com>
2025-04-16 19:44:26 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,6 +5,15 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_tb8788p1_64_wifi_k419.mk
+
+
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_tb8788p1_64_wifi_k419.mk
+
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_k6853v1_64_6360.mk
 
